f0053bc1eae96f16279783aa0b8182a0d9512108
minjae <alswo2722@naver.com>
2019-09-29 22:39:36 +0900

    이미지 추가

---
+++ collact/Utility/BaseVC.swift
@@ -0,0 +1,24 @@
+//
+//  BaseVC.swift
+//  collact
+//
+//  Created by minjae on 29/09/2019.
+//  Copyright © 2019 minjae. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
