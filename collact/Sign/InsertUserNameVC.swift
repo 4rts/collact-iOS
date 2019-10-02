30063c94e682db3f2676ff2639c2f287d7e795a3
minjae <alswo2722@naver.com>
2019-10-03 02:43:12 +0900

    이름 입력 퍼블리싱

---
+++ collact/Sign/InsertUserNameVC.swift
@@ -0,0 +1,18 @@
+//
+//  InsertUserNameVC.swift
+//  collact
+//
+//  Created by minjae on 30/09/2019.
+//  Copyright © 2019 minjae. All rights reserved.
+//
+
+import UIKit
+
+class InsertUserNameVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- collact/Sign/SignInVC.swift
+++ collact/Sign/SignInVC.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class SignInVC: BaseVC {
 
+    @IBOutlet weak var idTextField: MontTextFieldView!
+    
+    @IBOutlet weak var pwTextField: MontTextFieldView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        idTextField.textField.placeholder = "Id@email.com"
+        pwTextField.textField.placeholder = "Password"
+    }
+    
+    @IBAction func signInAction(_ sender: Any) {
+//        if idTextField.text != nil {
+//            self.view.makeToast("Please enter your ID")
+//        } else if pwTextField.text != nil {
+//            self.view.makeToast("Please enter your Password")
+//        } else {
+//             회원가입
+//        }
+    }
+    
+    @IBAction func signUpAction(_ sender: Any) {
+        
     }
     
 }
