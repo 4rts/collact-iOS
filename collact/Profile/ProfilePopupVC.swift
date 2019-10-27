c53b7780347b1abc28ce232458cf2b11af71579e
minjae <alswo2722@naver.com>
2019-10-28 01:21:26 +0900

    SwipeCard 중간까지 옴

--- collact/Profile/ProfilePopupVC.swift
+++ collact/Profile/ProfilePopupVC.swift
@@ -11,22 +11,41 @@ import UIKit
 class ProfilePopupVC: BaseVC {
     
     @IBOutlet weak var gradationColorView: UIView!
+    @IBOutlet weak var swipeCardContainerView: UIView!
     @IBOutlet weak var historyCountLabel: UILabel!
     @IBOutlet weak var collectorCountLabel: UILabel!
     @IBOutlet weak var chatableLabel: UILabel!
+    var swipeCardView: MJSwipeCardView!
     
+    override func loadView() {
+        super.loadView()
+        configureSwipeCardView()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setConstraintForDevice(swipeCardContainerView, NotchO: 420, NotchX: 340, identifier: "height")
     }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         topToBottomGradientColor(gradationColorView, colorArray: [UIColor(hex: "#ffffff"), UIColor(hex: "#e4e8ed")])
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    
+    
+    func configureSwipeCardView() {
+        swipeCardView = MJSwipeCardView()
+        swipeCardView.translatesAutoresizingMaskIntoConstraints = false
+        swipeCardContainerView.addSubview(swipeCardView)
         
-//         topToBottomGradientColor(gradationColorView, colorArray: [UIColor.red, UIColor.blue])
+        swipeCardView.topAnchor.constraint(equalTo: swipeCardContainerView.topAnchor).isActive = true
+        swipeCardView.leftAnchor.constraint(equalTo: swipeCardContainerView.leftAnchor).isActive = true
+        swipeCardView.rightAnchor.constraint(equalTo: swipeCardContainerView.rightAnchor).isActive = true
+        swipeCardView.bottomAnchor.constraint(equalTo: swipeCardContainerView.bottomAnchor).isActive = true
     }
+    
+    @IBAction func closeButtonAction(_ sender: Any) {
+        self.dismiss(animated: false, completion: nil)
+    }
+    
 
 }
