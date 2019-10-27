4f1b429ae984d81c3f928d30973f0afc904b69ca
minjae <alswo2722@naver.com>
2019-10-27 17:44:57 +0900

    프로필 팝업 시작

---
+++ collact/Profile/ProfilePopupVC.swift
@@ -0,0 +1,22 @@
+//
+//  ProfilePopupVC.swift
+//  collact
+//
+//  Created by minjae on 2019/10/27.
+//  Copyright © 2019 minjae. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePopupVC: UIViewController {
+
+    @IBOutlet weak var historyCountLabel: UILabel!
+    @IBOutlet weak var collectorCountLabel: UILabel!
+    @IBOutlet weak var chatableLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
